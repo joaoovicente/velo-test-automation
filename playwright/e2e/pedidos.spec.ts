25a53d741785af83b47dc98f0da9da9ddf443a28
João Miranda <jvmarroni99@gmail.com>
2026-02-13 02:06:13 -0300

    organizando dados de teste

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,6 +3,10 @@ import { test, expect } from '@playwright/test'
 /// AAA - Arrange(preparar), Act(agir), Assert(verificar)
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+  // Test Data
+  const order = 'VLO-WJLCUB'
+
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
@@ -11,11 +15,11 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill("VLO-WJLCUB")
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
   await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   // Assert
-  const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-WJLCUB"]')
+  const orderCode = page.locator(`//p[text()='Pedido']/..//p[text()='${order}']`)
   await expect(orderCode).toBeVisible({ timeout: 10000 })
 
   await expect(page.getByText('APROVADO')).toBeVisible()
