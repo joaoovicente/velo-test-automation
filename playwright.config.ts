11efcda61bcf5cb8a943d0b0ee2c7221138ec2e5
João Miranda <jvmarroni99@gmail.com>
2026-02-13 01:15:39 -0300

    melhorando código do desafio de localizadores e configurando timeouts corretamente

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,14 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 5000,
+  // tempo maximo de espera para cada teste
+  timeout: 60000,
+
+  // tempo maximo de espera para cada verificação de expectativa (toBeVisible, toContainText, etc)
+  expect: {
+    timeout: 5000 // não vale a pena aumentar porque o teste pode ficar lento no tempo de execução, vale a pena usar o timeout explicito
+  },
+
   testDir: './playwright/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -31,6 +38,14 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    // tempo maximo para ações interativas (clicks, fills, etc)
+    // Quando o valor é 0, herda o limite do timeout geral do teste
+    actionTimeout: 5000,
+
+    // tempo maximo para navegação (goto, reload, etc)
+    // Quando o valor é 0, herda o limite do timeout geral do teste
+    navigationTimeout: 10000
   },
 
   /* Configure projects for major browsers */
--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -10,17 +10,13 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-  const orderId = 'VLO-WJLCUB';
-  const orderStatus = 'APROVADO';
-
   // Act
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(orderId);
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill("VLO-WJLCUB")
   await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   // Assert
-  await expect(page.getByTestId(`order-result-${orderId}`)).toBeVisible({ timeout: 10000 })
-  await expect(page.getByTestId(`order-result-${orderId}`)).toContainText(orderId);
-  
-  await expect(page.getByTestId(`order-result-${orderId}`)).toBeVisible({ timeout: 10000 })
-  await expect(page.getByTestId(`order-result-${orderId}`)).toContainText(orderStatus);
+  const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-WJLCUB"]')
+  await expect(orderCode).toBeVisible({ timeout: 10000 })
+
+  await expect(page.getByText('APROVADO')).toBeVisible()
 })
